d6a7424948b99814df607634cd6d717757bdfac7
David Davis <davisd@davisd.com>
2011-11-08 13:31:02 -0600

    Updating MAX_SHOW_ALL_ALLOWED import to fix comatibility issues with django trunk

--- haystack/admin.py
+++ haystack/admin.py
@@ -1,6 +1,5 @@
 from django.contrib.admin.options import ModelAdmin
-from django.contrib.admin.views.main import (ChangeList, MAX_SHOW_ALL_ALLOWED,
-                                             SEARCH_VAR)
+from django.contrib.admin.views.main import ChangeList, SEARCH_VAR
 from django.core.exceptions import PermissionDenied, ImproperlyConfigured
 from django.core.paginator import Paginator, InvalidPage
 from django.shortcuts import render_to_response
@@ -13,7 +12,7 @@ try:
     from django.contrib.admin.options import csrf_protect_m
 except ImportError:
     from haystack.utils.decorators import method_decorator
-    
+
     # Do nothing on Django 1.1 and below.
     def csrf_protect(view):
         def wraps(request, *args, **kwargs):
@@ -22,7 +21,11 @@ except ImportError:
     
     csrf_protect_m = method_decorator(csrf_protect)
 
-
+try:
+    from django.contrib.admin.views.main import MAX_SHOW_ALL_ALLOWED
+except ImportError:
+    pass
+    
 class SearchChangeList(ChangeList):
     def get_results(self, request):
         if not SEARCH_VAR in request.GET:
@@ -36,7 +39,7 @@ class SearchChangeList(ChangeList):
         result_count = paginator.count
         full_result_count = SearchQuerySet().models(self.model).all().count()
         
-        can_show_all = result_count <= MAX_SHOW_ALL_ALLOWED
+        can_show_all = result_count <= getattr(self, 'list_max_show_all', MAX_SHOW_ALL_ALLOWED)
         multi_page = result_count > self.list_per_page
         
         # Get the list of objects to display on this page.
